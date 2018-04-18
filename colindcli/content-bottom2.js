822d558f5b954f945434129cdca0777f10ac48ab
colindcli <978478090@qq.com>
2018-04-18 21:19:12 +0800

    -

---
+++ colindcli/content-bottom2.js
@@ -0,0 +1,8 @@
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block; text-align:center;\'");
+document.writeln("     data-ad-layout=\'in-article\'");
+document.writeln("     data-ad-format=\'fluid\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'3725516153\'></ins>");
+
+(adsbygoogle = window.adsbygoogle || []).push({});
