c6ddac3af00af24d5273d2c8f5e4367c26d1a695
colindcli <978478090@qq.com>
2018-11-21 00:15:29 +0800

    -

---
+++ colindcli/content-bottom3.js
@@ -0,0 +1,9 @@
+document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-format=\'autorelaxed\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'2250999952\'></ins>");
+document.writeln("<script>");
+document.writeln("     (adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
