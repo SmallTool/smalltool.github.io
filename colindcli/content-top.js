3ce2cd4dda79d07e5b26ea4ba284e15c9c37e9f6
colindcli <978478090@qq.com>
2019-12-27 11:44:43 +0800

    -

--- colindcli/content-top.js
+++ colindcli/content-top.js
@@ -1,10 +1,8 @@
-// document.writeln("<ins class=\'adsbygoogle\'");
-// document.writeln("     style=\'display:inline-block;width:336px;height:280px\'");
-// document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
-// document.writeln("     data-ad-slot=\'2937040181\'></ins>");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;width:336px;height:280px\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'2937040181\'></ins>");
 
-// document.writeln("<script>");
-// document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
-// document.writeln("</script>");
-
-document.writeln("<a href=\"http://ms.fzxgj.top/?t=1\" target=\"_blank\" class=\"fancybox\"><img src=\"/img/ad.gif\" /></a>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
