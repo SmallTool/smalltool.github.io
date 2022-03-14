8459ab1fa263f70b25ae3e0de57c5a5cc49c39b3
COLIN <colindcli@users.noreply.github.com>
2022-03-15 01:03:22 +0800

    -

--- colindcli/content-bottom.js
+++
@@ -1,8 +0,0 @@
-document.writeln("<!-- smalltool-内容底部 -->");
-document.writeln("<ins class=\'adsbygoogle\'");
-document.writeln("     style=\'display:inline-block;width:336px;height:280px\'");
-document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
-document.writeln("     data-ad-slot=\'6289765971\'></ins>");
-document.writeln("<script>");
-document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
-document.writeln("</script>");
--- colindcli/content-top.js
+++
@@ -1,8 +0,0 @@
-document.writeln("<ins class=\'adsbygoogle\'");
-document.writeln("     style=\'display:inline-block;width:336px;height:280px\'");
-document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
-document.writeln("     data-ad-slot=\'2937040181\'></ins>");
-
-document.writeln("<script>");
-document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
-document.writeln("</script>");
