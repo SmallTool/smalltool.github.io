4352e9be8120db90d528c0f2696dfbbd50dc309d
COLIN <colindcli@users.noreply.github.com>
2022-03-15 01:13:27 +0800

    -

---
+++ colindcli/content-bottom.js
@@ -0,0 +1,8 @@
+﻿document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'6289765971\'");
+document.writeln("     data-ad-format=\'auto\'></ins>");
+document.writeln("<script>");
+document.writeln("     (adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
---
+++ colindcli/header.js
@@ -0,0 +1,8 @@
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
+document.writeln("     data-ad-slot=\'8930471380\'");
+document.writeln("     data-ad-format=\'auto\'></ins>");
+document.writeln("<script>");
+document.writeln("     (adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
