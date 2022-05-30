9c87e85b09bec66881a422556eea505016312766
COLIN <colindcli@users.noreply.github.com>
2022-05-30 17:23:15 +0800

    -

--- colindcli/content-bottom.js
+++ colindcli/content-bottom.js
@@ -1,4 +1,4 @@
-﻿document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("<ins class=\'adsbygoogle\'");
 document.writeln("     style=\'display:block\'");
 document.writeln("     data-ad-client=\'ca-pub-2222513431618339\'");
 document.writeln("     data-ad-slot=\'6289765971\'");
