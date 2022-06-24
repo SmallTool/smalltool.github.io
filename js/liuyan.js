4a1c0687d884676e49e6639f6099e92ace8fcca9
COLIN <colindcli@users.noreply.github.com>
2022-06-24 11:03:50 +0800

    -

--- js/liuyan.js
+++
@@ -1,6 +0,0 @@
-document.writeln("<div id=\"SOHUCS\" ></div> ");
-document.writeln("<script charset=\"utf-8\" type=\"text/javascript\" src=\"https://cy-cdn.kuaizhan.com/upload/changyan.js\" ></script>");
-window.changyan.api.config({
-    appid: 'cyvaJhbES',
-    conf: 'prod_ba0d4a413556ec0ad0957fa7213c476b'
-    });
